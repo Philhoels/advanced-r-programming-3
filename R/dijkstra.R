9c3c073aaec6767e71929f1788a39e3e575aed76
Ruben <RUBEN@MXROBOTS.COM>
2018-09-12 17:54:38 +0200

    inline documentation of dijkstra algorithm

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,26 +1,40 @@
+#' Dijkstra Algorithm
+#'
+#' @param graph  A data.frame, it has to consist of 3 vectors named 'v1', 'v2' and 'w' and all must have the same length as shown in the example.
+#' @param init_node A number.
+#'
+#' @return A vector consisting of the shortest paths from the node of origin "init_node" to the rest of the nodes. This is given ordering the nodes from smallest to largest.
+#' @export
+#'
+#' @examples
+#' wiki_graph <data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#' dijkstra(wiki_graph, 1)
+#' [1] 0 7 9 20 20 11
+
+
 dijkstra = function(graph, init_node) {
   if (!is.data.frame(graph) || length(graph) != 3) stop()
   if (!is.numeric(init_node)) stop()
   if (length(graph[1]) != length(graph[2]) || length(graph[1]) != length(graph[3])) stop()
-  
+
   # Algorithm
-  
+
   ## 1
   ### Creating a data frame containing information about if the edge has been visited and it's distance from init_node
   nodes = data.frame(unique(graph[1]),
                      rep(FALSE, times = length(graph[1])),
                      rep(Inf, times = length(graph[1])))
   names(nodes) = c("Edge", "Visited", "Distance")
-  
+
   print(nodes)
-  
+
   ## 2
   ### Set the distance to to init_node to 0 and set the current_node to init_node
   nodes[nodes$Edge == init_node, 3] = 0
   current_node = init_node
-  
+
   while (nrow(nodes[!nodes$Visited,]) > 0) {
-    
+
     ## 3
     ### Go through all unvisited neighbors and calculate the tentative distance
     for (i in 1:nrow(graph)) {
@@ -32,15 +46,15 @@ dijkstra = function(graph, init_node) {
         }
       }
     }
-    
+
     ## 4
     ### Mark as visited
     nodes[nodes$Edge == current_node, 2] = TRUE
-    
+
     ## 5
     ### If destination is marked visisted or the smallest unvisited node as infinity distance, stop
     unvisited.nodes = nodes[nodes$Visited == FALSE,]
-    
+
     if (length(unvisited.nodes[, 3]) != 0) {
       current_node = unvisited.nodes[unvisited.nodes[[3]] == min(unvisited.nodes[, 3]), 1]
       if (length(current_node) > 1) {
