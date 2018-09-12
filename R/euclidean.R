62c20213c418ed807fee8aa6660172a01c128c23
AnnalenaE <anner218@student.liu.se>
2018-09-12 17:41:33 +0200

    Add documentation code for eucledian

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,3 +1,13 @@
+#' Euclidean Algorithm
+#'
+#' @param a Any numeric value greater than 0
+#' @param b Any numeric value greater than 0
+#'
+#' @return The function returns the greatest common denominator of a and b.
+#' @export 
+#'
+#' @examples
+#' eucledian (6,22)
 euclidean = function(a, b){
   stopifnot(is.numeric(a) && is.numeric(b))
   while (b != 0) {
@@ -7,3 +17,4 @@ euclidean = function(a, b){
   }
   return(a)
 }
+
