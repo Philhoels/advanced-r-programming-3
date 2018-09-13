30620b831866f7c8cc4fa9484e2fc187233bb967
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-09-13 12:58:17 +0200

    Add documentation for wiki_graph

---
+++ R/wiki_graph.R
@@ -0,0 +1,12 @@
+#' Example Graph from Wikipedia
+#'
+#' A \code{data.frame} which can be used as an example for the dijkstra algorithm.
+#'
+#' @format A \code{data.frame} consisting of three vectors.
+#' \describe{
+#'   \item{v1}{First node of the edge.}
+#'   \item{v2}{Second node of the edge.}
+#'   \item{w}{Distance of the edge.}
+#' }
+#' @source https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+"wiki_graph"
